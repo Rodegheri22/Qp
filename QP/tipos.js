dc4f10a16d0c3da84ada7a7bece3976aa753a4a5
Kaio.r.rodrigues <110853592+Rodegheri22@users.noreply.github.com>
2022-12-07 16:02:51 -0300

    Add files via upload

---
+++ QP/tipos.js
@@ -0,0 +1,11 @@
+var x = "Alcides Garcia"
+
+var y = parseInt(67768734);
+
+var z = parseFloat(322,95);
+
+var w = parseInt(50);
+
+
+var soma = w + z
+document.write("Cliente:", x,   "    Conta:"    , y,   "    Saldo ="   ,z,   "Deposito = "   ,w,   "     Novo saldo ="   ,soma, );
